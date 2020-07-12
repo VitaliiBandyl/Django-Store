1eca2ea0af7b6ebdb586044001d4022839f42aaf
Vitalii Bandyl <vitaliibandyl@gmail.com>
2020-07-12 17:03:00 +0300

    Refactor HTML

--- django_store/settings.py
+++ django_store/settings.py
@@ -123,3 +123,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
