4ace77ba1e09e9d5db23d86c6a0a8b50508143de
Vitalii Bandyl <vitaliibandyl@gmail.com>
2020-07-12 21:00:10 +0300

    Customized admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,2 +1,77 @@
+from django import forms
 from django.contrib import admin
-from .models import Category, Subcategory, Product, ProductShot, Rating, RatingStar, Reviews
+
+from .models import Category, Product, ProductShot, Rating, Reviews
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class ProductAdminForm(forms.ModelForm):
+    """CKEditor form"""
+    description = forms.CharField(widget=CKEditorUploadingWidget())
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ReviewInline(admin.StackedInline):
+    """Reviews inline view"""
+    model = Reviews
+    extra = 1
+    readonly_fields = ('name', 'email', 'product')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Django admin category CRUD form"""
+    list_display = ('name', 'url')
+    list_display_links = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Django admin product CRUD form"""
+    list_display = ('title', 'category', 'year', 'country', 'price', 'availability', 'new',
+                    'top_selling', 'discount', 'created', 'updated')
+    list_filter = ('category', 'year', 'country', 'new', 'availability', 'top_selling')
+    search_fields = ('title', 'category__name')
+    inlines = [ReviewInline]
+    form = ProductAdminForm
+    save_on_top = True
+    save_as = True
+    list_editable = ('price', 'availability', 'new', 'top_selling', 'discount')
+    fieldsets = (
+        (None, {
+            'fields': (('title', 'url'),)
+    }),
+      (None, {
+            'fields': ('description', 'image')
+    }),
+        (None, {
+            'fields': (('category', 'country', 'year'),)
+    }),
+        (None, {
+            'fields': (('price', 'discount'),)
+    }),
+        (None, {
+            'fields': (('draft', 'new', 'availability', 'top_selling'),)
+        }),
+    )
+
+
+@admin.register(ProductShot)
+class ProductShotAdmin(admin.ModelAdmin):
+    """Django admin product shots CRUD form"""
+    list_display = ('title', 'product')
+
+
+@admin.register(Reviews)
+class ReviewsAdmin(admin.ModelAdmin):
+    """Django admin reviews CRUD form"""
+    list_display = ('name', 'email', 'product')
+    readonly_fields = ('name', 'email', 'product')
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    """Django admin rating CRUD form"""
+    list_display = ('product', 'star', 'ip')
--- store/models.py
+++ store/models.py
@@ -15,32 +15,21 @@ class Category(models.Model):
         verbose_name_plural = "Categories"
 
 
-class Subcategory(models.Model):
-    """Subcategories"""
-    name = models.CharField("Subcategory", max_length=150)
-    url = models.SlugField(max_length=150, unique=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL,  null=True)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = "Subcategory"
-        verbose_name_plural = "Subcategories"
-
-
 class Product(models.Model):
     """Goods"""
     title = models.CharField("Title", max_length=100)
     description = models.TextField("Description")
     image = models.ImageField("Image", upload_to="images/")
-    year = models.PositiveSmallIntegerField("Made date", default=2020)
+    year = models.PositiveSmallIntegerField("Year", default=2020)
     country = models.CharField("Country", max_length=30)
     price = models.DecimalField("Price", max_digits=10, decimal_places=2)
-    subcategory = models.ForeignKey(Subcategory, verbose_name="Subcategory", on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, verbose_name="Category", on_delete=models.CASCADE, null=True)
     url = models.SlugField(max_length=150, unique=True)
     draft = models.BooleanField("Draft", default=False)
     availability = models.BooleanField("Availability", default=True)
+    new = models.BooleanField('New', default=True)
+    top_selling = models.BooleanField('Top Selling', default=False)
+    discount = models.SmallIntegerField('Discount', default=0)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -75,7 +64,7 @@ class RatingStar(models.Model):
     value = models.SmallIntegerField("Star", default=0)
 
     def __str__(self):
-        return self.value
+        return str(self.value)
 
     class Meta:
         verbose_name = "Rating Star"
@@ -101,7 +90,7 @@ class Reviews(models.Model):
     email = models.EmailField()
     name = models.CharField("Name", max_length=100)
     text = models.TextField("Message", max_length=5000)
-    product = models.ForeignKey(Product, verbose_name="Movie", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name="Product", on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name} - {self.product}"
