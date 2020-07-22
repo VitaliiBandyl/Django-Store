2d67a6eefcdd9abcaa632f1c9b0927c2ee6934ea
Vitalii Bandyl <vitaliibandyl@gmail.com>
2020-07-22 19:23:59 +0300

    Some HTML markup fixes

--- store/views.py
+++ store/views.py
@@ -14,7 +14,7 @@ class HomeView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data()
-        context['popular_categories'] = Category.objects.filter(product__draft=False)[0:3]
+        context['popular_categories'] = Category.objects.filter(product__draft=False).distinct()[0:3]
         return context
 
 
